36a7decb25d879f0f09582f7b36dae7b723879b3
Nancy Manoj Alkari <nancy.alkari9349.myunfc.ca>
2026-03-01 22:16:56 -0500

    US-5: Implement grouping logic for top 10 customers

--- customer.py
+++ customer.py
@@ -1,7 +1,14 @@
 import pandas as pd
 
 def top_10_customers(df):
-    """
-    Returns top 10 customers based on total sales.
-    """
-    pass
+    df["Sales"] = pd.to_numeric(df["Sales"], errors="coerce")
+
+    result = (
+        df.groupby("Customer Name")["Sales"]
+        .sum()
+        .sort_values(ascending=False)
+        .head(10)
+        .reset_index()
+    )
+
+    return result
